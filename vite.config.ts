fedbc7a3cd22fe888a25febe7f4c4a01292a24d0
elbok <kromsten@pm.me>
2023-04-20 20:07:56 +0200

    crypto + polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import { resolve } from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), nodePolyfills({protocolImports: true}) ],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	resolve: {
+		alias: {
+			'@components': resolve('./src/components'),
+			'@lib': resolve('./src/lib'),
+			'@stores': resolve('./src/stores'),
+			'@routes': resolve('./src/routes'),
+			'@interfaces': resolve('./src/interfaces'),
+		}
 	}
 });
