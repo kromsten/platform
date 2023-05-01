ba9c348b3671bac65e56f894be381c8be09c836b
elbok <kromsten@pm.me>
2023-05-01 22:28:18 +0200

    auto deploy and config

---
+++ integration_tests/index.test.ts
@@ -0,0 +1,97 @@
+import { readFileSync, readdirSync, writeFileSync } from "fs";
+import { sha256 } from "@noble/hashes/sha256";
+import { toHex, MsgStoreCode, TxResultCode, MsgInstantiateContractResponse } from "secretjs";
+import importedConfig from "./assets/config.json";
+import { Account, getAccount } from "./wallets.js";
+import { expect } from "bun:test";
+
+
+
+type ContractConfig = {
+    address?: string;
+    codeId?: number;
+    codeHash?: string;
+}
+
+type Config = {
+    contracts: {
+        [key: string]: ContractConfig;
+    }
+}
+
+
+const ASSET_PATH = "./assets";
+const WASM_PATH = `${ASSET_PATH}/wasm`;
+const config = importedConfig as Config;
+
+
+const mainAccount : Account =  getAccount();
+const client = mainAccount.secretjs;
+
+const loadContracts = async () => {
+
+    const files = readdirSync(WASM_PATH);
+
+
+    for (const file of files) {
+
+        const name = file.split(".")[0];
+
+        if (!config.contracts[name]?.codeId) {
+            const wasm = readFileSync(`${WASM_PATH}/${file}`) as Uint8Array;
+            const codeHash = toHex(sha256(wasm));
+            
+            const msg = new MsgStoreCode({
+                sender: mainAccount.address,
+                wasm_byte_code: wasm,
+                source: "",
+                builder: "",
+            })
+
+            const tx = await client.tx.broadcast([msg], {gasLimit: 5_000_000});
+
+            if (tx.code !== TxResultCode.Success) {
+                console.error(tx.rawLog);
+            }
+
+            expect(tx.code).toBe(TxResultCode.Success);
+            const codeId = Number(tx.arrayLog!.find((x) => x.key === "code_id")!.value);
+            
+            config.contracts[name].codeId = codeId;
+            config.contracts[name].codeHash = codeHash;
+        }
+
+
+        if (!config.contracts[name]?.address) {
+
+            const { codeId, codeHash } = config.contracts[name];
+
+            const tx = await client.tx.compute.instantiateContract(
+                {
+                  sender: mainAccount.address,
+                  code_id: codeId!,
+                  code_hash: codeHash!,
+                  init_msg: {},
+                  label: `${name}-${Date.now()}`,
+                },
+                { gasLimit: 300_000 }
+              );
+
+            expect(tx.code).toBe(TxResultCode.Success);
+            const address = MsgInstantiateContractResponse.decode(tx.data[0]).address;
+            config.contracts[name].address = address;
+        }
+
+        writeFileSync(`${ASSET_PATH}/config.json`, JSON.stringify(config, null, 2));
+    }
+
+}
+
+
+const init = async () => {
+    await loadContracts();
+}
+
+
+console.log("Starting integration tests...");
+await init();
---
+++ integration_tests/wallets.ts
@@ -0,0 +1,40 @@
+import { SecretNetworkClient, Wallet } from "secretjs";
+
+export type Account = {
+    address: string;
+    mnemonic: string;
+    wallet: Wallet;
+    secretjs: SecretNetworkClient;
+  };
+
+
+const a = "grant rice replace explain federal release fix clever romance raise often wild taxi quarter soccer fiber love must tape steak together observe swap guitar";
+const b = "jelly shadow frog dirt dragon use armed praise universe win jungle close inmate rain oil canvas beauty pioneer chef soccer icon dizzy thunder meadow";
+const c = "chair love bleak wonder skirt permit say assist aunt credit roast size obtain minute throw sand usual age smart exact enough room shadow charge";
+
+const mnemonics = [a, b, c];
+
+const accounts: Account[] = new Array<Account>(3);
+
+for (let i = 0; i < mnemonics.length; i++) {
+    const mnemonic = mnemonics[i];
+    const wallet = new Wallet(mnemonic, {
+        
+    });
+    accounts[i] = {
+        address: wallet.address,
+        mnemonic: mnemonic,
+        wallet,
+        secretjs: new SecretNetworkClient({
+            url: "http://localhost:9091",
+            wallet: wallet,
+            walletAddress: wallet.address,
+            chainId: "secretdev-1",
+        }),
+    };
+
+}
+
+export const getAccount = (index : 0 | 1 | 2 = 0) => {
+    return accounts[index];
+}
