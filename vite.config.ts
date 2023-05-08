02af046832029b07d50d38b555cc04d5ddedfbe3
elbok <kromsten@pm.me>
2023-05-08 11:32:54 +0200

    alias change for vscode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import { resolve } from 'path';
 
 export default defineConfig({
 	plugins: [sveltekit(), nodePolyfills({protocolImports: true}) ],
@@ -9,12 +8,4 @@ export default defineConfig({
 		include: ['*tests*/**/*.{test,spec}.{js,ts}'],
 		testTimeout: 20000
 	},
-	resolve: {
-		alias: {
-			'@components': resolve('./src/components'),
-			'@lib': resolve('./src/lib'),
-			'@routes': resolve('./src/routes'),
-			'@interfaces': resolve('./src/interfaces'),
-		}
-	}
 });
